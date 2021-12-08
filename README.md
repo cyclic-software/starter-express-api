084cfed12dac40272088625a56e4aae4925d41a4
Mike <korostelevm@gmail.com>
2021-12-08 06:53:29 -0500

    added express api

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # starter-express-api
+
+This is the simplest possible nodejs api using express that responds to any request with: 
+```
+Yo!
+```
---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
