89604ef56b551a388560f231ea9fca43722a0223
DevokSoftware <devok.software@gmail.com>
2022-10-23 19:00:51 +0200

    bolaaoar - backend deploy

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const standingsJson = require('./standings.json')
+app.get('/stats', async (req, res) => {
+    console.log('Requesting stats...')
+    NBA.stats.teamStats({ Season: '2022-23' }).then(x => {
+        console.log("Sending stats...")
+        res.send(x)
+    });
 })
-app.listen(process.env.PORT || 3000)
+
+app.get('/standings', (req, res) => {
+    console.log('Requesting standings...')
+    res.send(standingsJson)
+})
+app.listen(process.env.PORT || 3001)
