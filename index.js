7c20bb7b844ee89c57a7659e788b42f55dc96883
DevokSoftware <devok.software@gmail.com>
2022-10-23 19:46:24 +0200

    added cors to server endpoints

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
 const standingsJson = require('./standings.json')
+var cors = require('cors');
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+
+var allowedOrigins = ['http://localhost:3000',
+    'https://draft-bola-ao-ar.onrender.com/'];
+
 app.get('/stats', async (req, res) => {
     console.log('Requesting stats...')
     NBA.stats.teamStats({ Season: '2022-23' }).then(x => {
