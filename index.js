2ac8785a06ceac99637d53a36206c802ff4e71a8
DevokSoftware <devok.software@gmail.com>
2022-10-26 23:51:25 +0200

    changed cron hours and removed unnecessary logs

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors({
 var allowedOrigins = ['http://localhost:3000',
     'https://draft-bola-ao-ar.onrender.com'];
 
-cron.schedule("0 * * * *", function () {
+cron.schedule("0 20,22,23,0,1,2,3,4,5,6,7,8, * * *", function () {
     console.log("Updating Standings...");
     const options = {
         method: 'GET',
@@ -36,7 +36,6 @@ cron.schedule("0 * * * *", function () {
 
     request('https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022', options, function (error, response, body) {
         console.log('Received response from API.');
-        console.log(filePath)
         if (!error && response.statusCode == 200) {
             json = JSON.parse(response.body)
             json.lastUpdate = new Date().toLocaleString("pt-PT")
@@ -56,8 +55,7 @@ cron.schedule("0 * * * *", function () {
 
 
 app.get('/standings', (req, res) => {
-    console.log('Requesting standings...')
-    console.log(filePath)
+    console.log('GET - Requesting standings...')
     fs.readFile(filePath, function (err, data) {
         if (!err) {
             res.send(JSON.parse(data))
