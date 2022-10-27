cf66904982f84c1ea08aeb481738a0045baeaa9f
DevokSoftware <devok.software@gmail.com>
2022-10-27 22:36:13 +0200

    removed cron jobs. file is now updated when a request is made

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 var cors = require('cors');
 const cron = require('node-cron')
 const request = require('request');
+const moment = require('moment')
 const fs = require('fs');
 path = require('path'),
     filePath = path.join(__dirname, 'standings.json');
@@ -24,41 +25,44 @@ app.use(cors({
 var allowedOrigins = ['http://localhost:3000',
     'https://draft-bola-ao-ar.onrender.com'];
 
-cron.schedule("30 20,22,23,0,1,2,3,4,5,6,7,8,10,11 * * *", function () {
-    console.log("Updating Standings...");
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
-            'x-rapidapi-key': process.env.API_KEY
-        }
-    };
-
-    request('https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022', options, function (error, response, body) {
-        console.log('Received response from API.');
-        if (!error && response.statusCode == 200) {
-            json = JSON.parse(response.body)
-            json.lastUpdate = new Date().toLocaleString("pt-PT")
-            fs.writeFile(filePath, JSON.stringify(json), function (err) {
-                if (err) {
-                    console.log(err);
-                    throw err
-                };
-                console.log('Standings file was updated!');
-            }
-            );
-        } else {
-            console.log(response.body);
-        }
-    })
-})
-
-
-app.get('/standings', (req, res) => {
+app.get('/standings', async (req, res) => {
     console.log('GET - Requesting standings...')
     fs.readFile(filePath, function (err, data) {
         if (!err) {
-            res.send(JSON.parse(data))
+            const jsonFile = JSON.parse(data);
+            const fileDate = moment(jsonFile.lastUpdate, 'DD-MM-YYYY HH:mm:ss')
+            if (fileDate.add(2, 'hour').isBefore(moment())) {
+
+                console.log("Updating Standings...");
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+                        'x-rapidapi-key': process.env.API_KEY
+                    }
+                };
+                request('https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022', options, function (error, response, body) {
+                    console.log('Received response from API.');
+                    if (!error && response.statusCode == 200) {
+                        json = JSON.parse(response.body)
+                        json.lastUpdate = moment().format('DD-MM-YYYY HH:mm:ss')
+                        fs.writeFile(filePath, JSON.stringify(json), function (err) {
+                            if (err) {
+                                console.log(err);
+                                throw err
+                            };
+                            console.log('Standings file was updated!');
+                        }
+
+                        );
+                        res.send(JSON.stringify(json))
+                    } else {
+                        console.log(response.body);
+                    }
+                })
+            } else {
+                res.send(jsonFile)
+            }
         } else {
             console.log(err);
         }
