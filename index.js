f46cb84cc04721bb3e9b4ecc8848217250ace58d
fajarot <59015737+fajarot@users.noreply.github.com>
2022-11-17 07:50:05 +0000

    add get user

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/api/users', function(req, res){
+    const user_id = req.query.id;
+    const token = req.query.token;
+    const geo = req.query.geo;
+
+    res.send({
+        'user_id': user_id,
+        'token': token,
+        'geo': geo,
+    });
+});
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port)
