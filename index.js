c66f01fe74f56123ca475a12367402819857ed6e
fajarot <59015737+fajarot@users.noreply.github.com>
2022-11-17 23:51:09 +0000

    branch init

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const axios = require('axios')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/api/users', function(req, res){
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// get data from query ?&&
+app.get('/api/users', function (req, res) {
     const user_id = req.query.id;
     const token = req.query.token;
     const geo = req.query.geo;
@@ -13,6 +18,70 @@ app.get('/api/users', function(req, res){
         'token': token,
         'geo': geo,
     });
+});
+
+app.get('/api/data', function(req, res){
+    let config = {
+        method: 'get',
+        url: 'https://api.quotable.io/random',
+    };
+
+    console.log('start');
+    axios(config)
+        .then(function(response){
+            // console.log(response.data)
+            res.send(response.data)
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+})
+
+app.get('/api/data', function(req, res){
+    let config = {
+        method: 'get',
+        url: 'https://api.quotable.io/random',
+    };
+
+    console.log('start');
+    axios(config)
+        .then(function(response){
+            // console.log(response.data)
+            res.send(response.data)
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+})
+
+
+// modif parameter
+app.param('name', function (req, res, next, name) {
+    const modified = name.toUpperCase();
+
+    req.name = modified;
+    next();
+})
+app.get('/api/users/:name', function (req, res) {
+    res.send('Hello ' + req.name + '!!!');
+})
+
+
+// post data from body from-urlencoded
+app.post('/api/users', function (req, res) {
+    const user_id = req.body.id;
+    const token = req.body.token;
+    const geo = req.body.geo;
+
+    res.send({
+        'user_id': user_id,
+        'token': token,
+        'geo': geo
+    });
+});
+
+app.get('/api/:version', function (req, res) {
+    res.send(req.params.version);
 });
 
 app.all('/', (req, res) => {
