0788c97ecb61cc5e38d51502a6b27c72a95cb7bc
dimavalencko <dima.val41@gmail.com>
2022-11-30 14:22:40 +0300

    Fix

--- clients/telr.js
+++ clients/telr.js
@@ -2,97 +2,129 @@ const qrcode = require('qrcode');
 const axios = require('axios');
 
 class Telr {
-    #authkey;
-    #storeId;
-	#qlApi;
+  #authkey;
+  #storeId;
+  #qlApi;
 
-    constructor(authKey, storeId, creteQLApi){
-		this.#authkey = authKey;
-		this.#storeId = storeId;
-		this.#qlApi = creteQLApi;
-        quickLinksData.QuickLinkRequest.StoreID = storeId;
-        quickLinksData.QuickLinkRequest.AuthKey = authKey;
-    }
-
-    createQuickLink([date, amount, name]) {
-      return new Promise(async (resolve, reject) => {
-        // Copy QuickLinksData object
-        let _qld = JSON.stringify(quickLinksData);
-        let qlData = JSON.parse(_qld);
-      
-        qlData.QuickLinkRequest.Details.Desc = `${date} ${name}`;
-        qlData.QuickLinkRequest.Details.Amount = amount;
-        qlData.QuickLinkRequest.Details.FullName = name;
-
-        try {
-          const request = await axios.post(this.#qlApi, qlData);
-          const qrUrl = request.data.QuickLinkResponse.URL;
-          resolve (await this.createQRCode(qrUrl));
-        } catch(e) {
-          reject(e);
-        }
-      });
-    }
-
-	createQRCode(qrUrl) {
-		return new Promise((resolve, reject) => {
-			qrcode.toDataURL(qrUrl, (err, url) => {
-				if (err) {
-					reject(err);
-				}
-				let base64Url = url.replace(new RegExp(`.*?${','}(.*)`), '$1');
-				resolve({qrCode: Buffer.from(base64Url, 'base64'), url: qrUrl});
-			})
-		});
+  constructor(authKey, storeId, creteQLApi) {
+    this.#authkey = authKey;
+    this.#storeId = storeId;
+    this.#qlApi = creteQLApi;
   }
-}
 
-const quickLinksData = {
-	"QuickLinkRequest": {
-		"StoreID": '',
-		"AuthKey": '',
-		"Details": {
-			"Desc": "",
-			"Cart": "",
-			"Currency": "AED",
-			"Amount": "",
-			"MinQuantity": "1",
-			"MaxQuantity": "1",
-			"FullName": "",
-			"Addr1": "",
-			"City": "",
-			"Country": "RU",
-			"Email": "",
-			"Phone": ""
-		},
-		"VariableValueMode": {
-			"Status": "",
-			"SectionTitle": ""
-		},
-		"RepeatBilling": {
-			"Status": "",
-			"Amount": "",
-			"Period": "",
-			"Interval": "",
-			"Start": "",
-			"Term": "",
-			"Final": ""
-		},
-		"Availability": {
-			"NotValidBefore": {
-				"Day": "",
-				"Month": "",
-				"Year": ""
-			},
-			"NotValidAfter": {
-				"Day": "",
-				"Month": "",
-				"Year": ""
-			},
-			"StockControl": "1",
-			"StockCount": "5"
-		}
-	}
+  createQuickLink([date, amount, name]) {
+    return new Promise(async (resolve, reject) => {
+      const quickLinksData = {
+        QuickLinkRequest: {
+          StoreID: '',
+          AuthKey: '',
+          Details: {
+            Desc: "",
+            Cart: "",
+            Currency: "AED",
+            Amount: "",
+            MinQuantity: "1",
+            MaxQuantity: "1",
+            FullName: "",
+            Addr1: "",
+            City: "",
+            Country: "RU",
+            Email: "",
+            Phone: ""
+          },
+          VariableValueMode: {
+            Status: "",
+            SectionTitle: ""
+          },
+          RepeatBilling: {
+            Status: "",
+            Amount: "",
+            Period: "",
+            Interval: "",
+            Start: "",
+            Term: "",
+            Final: ""
+          },
+          Availability: {
+            NotValidBefore: {
+              Day: "",
+              Month: "",
+              Year: ""
+            },
+            NotValidAfter: {
+              Day: "",
+              Month: "",
+              Year: ""
+            },
+            StockControl: "1",
+            StockCount: "5"
+          }
+        }
+      }
+
+      quickLinksData.QuickLinkRequest.StoreID = storeId;
+      quickLinksData.QuickLinkRequest.AuthKey = authKey;
+      quickLinksData.QuickLinkRequest.Details.Desc = `${date} ${name}`;
+      quickLinksData.QuickLinkRequest.Details.Amount = amount;
+      quickLinksData.QuickLinkRequest.Details.FullName = name;
+
+      try {
+        const request = await axios.post(this.#qlApi, quickLinksData);
+        const qrUrl = request.data.QuickLinkResponse.URL;
+        resolve(await this.createQRCode(qrUrl));
+      }
+      catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  createQRCode(qrUrl) {
+    return new Promise((resolve, reject) => {
+      qrcode.toDataURL(qrUrl, (err, url) => {
+        if (err) {
+          reject(err);
+        }
+        const base64Url = url.replace(new RegExp(`.*?${','}(.*)`), '$1');
+        resolve({ qrCode: Buffer.from(base64Url, 'base64'), url: qrUrl });
+      })
+    });
+  }
+
+  getTransacionInfo(ref) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const transInfo = await axios.get(process.env.GET_TRANSACTION_API + `/${ref}`, {
+          headers: {
+            'Authorization': process.env.AUTHORIZATION_TOKEN
+          }
+        });
+        resolve(transInfo.data);
+      }
+      catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  defineTransactionStatus(code) {
+    let status = 'undefined';
+    switch (code) {
+      case 'A':
+        status = 'Успешная оплата';
+        break;
+      case 'H':
+        status = 'Оплата в ожидании';
+        break;
+      case 'E':
+        status = 'Ошибка сервера';
+        break;
+      default:
+        status = 'undefined'
+        break;
+    }
+    return status;
+  }
 }
 
 module.exports = Telr;
--- index.js
+++ index.js
@@ -2,21 +2,19 @@ const TelegramApi = require('node-telegram-bot-api');
 const express = require('express');
 const bodyParser = require('body-parser');
 const Telr = require('./clients/telr');
-const moment = require('moment-timezone');
+const moment = require('moment');
 const axios = require('axios');
 const parseString = require('xml2js').parseString;
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.text({ type: "*/*" }));
 const bot = new TelegramApi(process.env.TELEGRAM_BOT_TOKEN, { polling: false });
 const telr = new Telr(process.env.AUTH_KEY, process.env.STORE_ID, process.env.CREATE_QUICKLINK_API);
 const botName = process.env.TELEGRAM_BOT_NAME;
 
 // Endpoints
 app.get('/', async (request, response) => {
-  if (!request.query) return response.sendStatus(400);
-  response.send('OK');
+  response.send('ECHO');
 })
 
 app.post('/', async (request, response) => {
@@ -25,13 +23,14 @@ app.post('/', async (request, response) => {
   }
 
   const msg = request.body.message;
-  if (/\/start/.test(msg.text)) {
-    await bot.sendMessage(msg.chat.id, 'Привет! Я Телеграм бот для генерации qr-кода для оплаты услуг и товаров. Пожалуйста, введите данные в формате "дата(дд.мм)/сумма/имя" для создания ссылки на оплату.');
+  const chatId = msg.chat.id;
+
+  if (process.env.START_COMMAND_REG.test(msg.text)) {
+    await bot.sendMessage(chatId, process.env.START_COMMAND_TEXT);
     return response.sendStatus(200);
   }
 
-  if (RegExp(/\b\s\d{1,2}\.\d{1,2}\/[+-]?([0-9]*[.,])?[0-9]+\/[A-zА-я]+/g).test(msg.text)) {
-    const chatId = msg.chat.id;
+  if (CREATE_QUICKLINK_COMMAND_REG.test(msg.text)) {
     const data = msg.text.split(' ');
 
     console.log(data);
@@ -44,65 +43,56 @@ app.post('/', async (request, response) => {
 
     if (!moment(date, 'DD.MM.YYYY', true).isValid()) {
       console.log('The entered date is not valid', date);
-      return await bot.sendMessage(chatId, `Ведённая дата (${date}) не существует. Проверьте правильность даты и повторите попытку снова!`);
+      await bot.sendMessage(chatId, `Ведённая дата (${date}) не существует. Проверьте правильность даты и повторите попытку снова!`);
     }
 
     // For a negative payment amount
     if (+amount < 0) {
       return await bot.sendMessage(chatId, 'Сумма платежа не может быть меньше 0!');
     }
-    if (amount.includes(',')) {
-      return await bot.sendMessage(chatId, `Указанная сумма должна быть разделена точкой, а не запятой (${amount.replace(',', '.')})`);
+    if (!amount.includes(',')) {
+      const qlData = await telr.createQuickLink([date, amount, name]);
+      let opts = { 'caption': qlData.url.replace('_', '\\_'), 'parse_mode': 'markdown' }; // The '_' character must be escaped, otherwise there will be an error
+      await bot.sendPhoto(chatId, qlData.qrCode, opts);
     }
-
-    let qlData = await telr.createQuickLink([date, amount, name]);
-    let opts = { 'caption': qlData.url.replace('_', '\\_'), 'parse_mode': 'markdown' }; // The '_' character must be escaped, otherwise there will be an error
-    await bot.sendPhoto(chatId, qlData.qrCode, opts);
-
-    return response.sendStatus(200);
+    else await bot.sendMessage(chatId, `Указанная сумма должна быть разделена точкой, а не запятой (${amount.replace(',', '.')})`);
   }
-
   return response.sendStatus(200);
 });
 
 app.post('/payment_gate', async (request, response) => {
-  if (!request.body) return response.sendStatus(400);
+  if (!request.body) return response.sendStatus(200);
   const data = request.body.split('&');
   let ref = '';
   data.forEach(el => {
     if (el.startsWith('tran_ref')) {
-      console.log(el)
       ref = el.split('=')[1];
     }
   });
-  const getTranInfo = await axios.get(`https://secure.telr.com/tools/api/xml/transaction/${ref}`, {
-    headers: {
-      'Authorization': process.env.AUTHORIZATION_TOKEN
-    }
-  });
-  parseString(getTranInfo.data, async function (err, trc) {
-    let id = trc.transaction.id[0];
-    let srcDate = moment.tz(trc.transaction.date[0], "Asia/Dubai");
-    let serverDate = srcDate.format('YYYY-MM-DD HH:mm:ss');
-    let status = '';
-    switch (trc.transaction.auth[0].status[0]) {
-      case 'A':
-        status = 'Успешная оплата';
-        break;
-      case 'H':
-        status = 'Оплата в ожидании';
-        break;
-      case 'E':
-        status = 'Ошибка сервера';
-        break;
-      default:
-        status = 'undefined'
-        break;
-    }
 
-    await bot.sendMessage(process.env.TELEGRAM_GROUP_ID, `<b>Информация по платежу:</b>\n<b>ID транзакции:</b> ${id}\n<b>Дата Дубай, ОАЭ:</b> ${serverDate}\n<b>Статус платежа:</b> ${status}`, { parse_mode: 'HTML' });
-  });
+  const transactionInfo = telr.getTransacionInfo(ref);
+  try {
+    parseString(transactionInfo, async function (err, trc) {
+      let id = trc.transaction.id[0];
+      let srcDate = moment.tz(trc.transaction.date[0], "Asia/Dubai");
+      let serverDate = srcDate.format('YYYY-MM-DD HH:mm:ss');
+      const status = telr.defineTransactionStatus(trc.transaction.auth[0].status[0]);
+
+      await bot.sendMessage(
+        process.env.TELEGRAM_GROUP_ID,
+        `<b>Информация по платежу:</b>\n
+          <b>ID транзакции:</b> ${id}\n
+          <b>Дата Дубай, ОАЭ:</b> ${serverDate}\n
+          <b>Статус платежа:</b> ${status}`,
+        { parse_mode: 'HTML' }
+      );
+    });
+  }
+  catch (error) {
+    return response.status(200).send(error);
+  }
+  return response.status(200).send(error);
 })
 
 // Express server logic
-app.listen(3000, async () => console.log(`App listening on port ${process.env.EXPRESS_PORT}`))
+app.listen(process.env.EXPRESS_PORT, async () => console.log(`App listening on port ${process.env.EXPRESS_PORT}`))
