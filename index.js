272261edc1ed02a8c023e5800689ff307668cc72
Дима <dima.valencko@yandex.ru>
2022-11-30 15:44:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,65 +20,71 @@ const startCommandReg = RegExp(/\/start/);
 const creatQLCommandReg = RegExp(/\b\s\d{1,2}\.\d{1,2}\/[+-]?([0-9]*[.,])?[0-9]+\/[A-zА-я]+/g);
 // Endpoints
 app.get('/', async (request, response) => {
-  response.send('ECHO');
+  response.status(200).send('ECHO');
 })
 
 app.post('/', async (request, response) => {
-  if (!request.body.hasOwnProperty('update_id')) {
+  try {
+    if (!request.body.hasOwnProperty('update_id')) {
+      return response.sendStatus(200);
+    }
+  
+    const msg = request.body.message;
+    console.log('Message: ', msg)
+    const chatId = msg.chat.id;
+  
+    if (startCommandReg.test(msg.text)) {
+      await bot.sendMessage(chatId, process.env.START_COMMAND_TEXT);
+      return response.sendStatus(200);
+    }
+  
+    if (creatQLCommandReg.test(msg.text)) {
+      const data = msg.text.split(' ');
+  
+      console.log(data);
+  
+      if (data[0] !== botName) return response.sendStatus(200);
+  
+      const paymentData = data[1].split('/');
+      let [date, amount, name] = paymentData;
+      date += '.' + new Date().getFullYear();
+  
+      if (!moment(date, 'DD.MM.YYYY', true).isValid()) {
+        console.log('The entered date is not valid', date);
+        await bot.sendMessage(chatId, `Ведённая дата (${date}) не существует. Проверьте правильность даты и повторите попытку снова!`);
+      }
+  
+      // For a negative payment amount
+      if (+amount < 0) {
+        await bot.sendMessage(chatId, 'Сумма платежа не может быть меньше 0!');
+      }
+      if (!amount.includes(',')) {
+        const qlData = await telr.createQuickLink([date, amount, name]);
+        let opts = { 'caption': qlData.url.replace('_', '\\_'), 'parse_mode': 'markdown' }; // The '_' character must be escaped, otherwise there will be an error
+        await bot.sendPhoto(chatId, qlData.qrCode, opts);
+      }
+      else await bot.sendMessage(chatId, `Указанная сумма должна быть разделена точкой, а не запятой (${amount.replace(',', '.')})`);
+    }
+  } catch (error) {
+    return response.status(200).send(err);
+  }
+  finally{
     return response.sendStatus(200);
   }
-
-  const msg = request.body.message;
-  console.log('Message: ', msg)
-  const chatId = msg.chat.id;
-
-  if (startCommandReg.test(msg.text)) {
-    await bot.sendMessage(chatId, process.env.START_COMMAND_TEXT);
-    return response.sendStatus(200);
-  }
-
-  if (creatQLCommandReg.test(msg.text)) {
-    const data = msg.text.split(' ');
-
-    console.log(data);
-
-    if (data[0] !== botName) return response.sendStatus(200);
-
-    const paymentData = data[1].split('/');
-    let [date, amount, name] = paymentData;
-    date += '.' + new Date().getFullYear();
-
-    if (!moment(date, 'DD.MM.YYYY', true).isValid()) {
-      console.log('The entered date is not valid', date);
-      await bot.sendMessage(chatId, `Ведённая дата (${date}) не существует. Проверьте правильность даты и повторите попытку снова!`);
-    }
-
-    // For a negative payment amount
-    if (+amount < 0) {
-      return await bot.sendMessage(chatId, 'Сумма платежа не может быть меньше 0!');
-    }
-    if (!amount.includes(',')) {
-      const qlData = await telr.createQuickLink([date, amount, name]);
-      let opts = { 'caption': qlData.url.replace('_', '\\_'), 'parse_mode': 'markdown' }; // The '_' character must be escaped, otherwise there will be an error
-      await bot.sendPhoto(chatId, qlData.qrCode, opts);
-    }
-    else await bot.sendMessage(chatId, `Указанная сумма должна быть разделена точкой, а не запятой (${amount.replace(',', '.')})`);
-  }
-  return response.sendStatus(200);
 });
 
 app.post('/payment_gate', async (request, response) => {
   if (!request.body) return response.sendStatus(200);
-  const data = request.body.split('&');
-  let ref = '';
-  data.forEach(el => {
-    if (el.startsWith('tran_ref')) {
-      ref = el.split('=')[1];
-    }
-  });
-
-  const transactionInfo = telr.getTransacionInfo(ref);
   try {
+    const data = request.body.split('&');
+    let ref = '';
+    data.forEach(el => {
+      if (el.startsWith('tran_ref')) {
+        ref = el.split('=')[1];
+      }
+    });
+
+    const transactionInfo = telr.getTransacionInfo(ref);
     parseString(transactionInfo, async function (err, trc) {
       let id = trc.transaction.id[0];
       let srcDate = moment.tz(trc.transaction.date[0], "Asia/Dubai");
@@ -98,7 +104,9 @@ app.post('/payment_gate', async (request, response) => {
   catch (error) {
     return response.status(200).send(error);
   }
-  return response.status(200).send(error);
+  finally{
+    return response.sendStatus(200);
+  }
 })
 
 // Express server logic
