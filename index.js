860287602f26ff64bf525ee07cd8fedbec957cb8
Felipe-M-Duarte <117078988+Felipe-M-Duarte@users.noreply.github.com>
2022-12-17 10:24:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('<h1>ROTA FUNCIONADO</h1>')
 })
 app.listen(process.env.PORT || 3000)
