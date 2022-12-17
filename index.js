a8bd5658c386ad35b90ad001325bcdc87f311873
Felipe-M-Duarte <117078988+Felipe-M-Duarte@users.noreply.github.com>
2022-12-17 14:46:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('<h1>ROTA FUNCIONADO</h1>')
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes.js';
+
+const dbUrl = "mongodb+srv://FelipeMDuarte:35624652Fenix-@cluster0.jl61ifv.mongodb.net/?retryWrites=true&w=majority";
+const ConnectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(dbUrl, ConnectionParams).then(()=>{
+    console.log("Foi conectado com sucesso")
+}).catch((error)=>{
+    console.log("Um defeito ocorreu: em quanto conectava", error)
 })
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+
 app.listen(process.env.PORT || 3000)
