54bec860607ac23d448caf6aaf0de8b274a12b95
Felipe-M-Duarte <117078988+Felipe-M-Duarte@users.noreply.github.com>
2023-01-02 10:33:44 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import contactController from './controller/contactController.js';
 import HomeContactController from './controller/HomeContactController.js';
+import SignupCustomerController from './controller/SignupCustomerController.js';
 
 const routes = Router();
 
 routes.post("/contact", contactController.create)
 
 routes.post("/contacthome", HomeContactController.create)
+
+routes.post("/contacthome", HomeContactController.create)
+
+routes.post("/admin/cadastrar/cliente", SignupCustomerController.create)
 
 routes.get("/", (req, res) => {
     res.send('<h1>ROTA FUNCIONADO</h1>')
