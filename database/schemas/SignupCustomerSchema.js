c23db95135eb9db2734b067ebe3517ea24d46f16
Felipe-M-Duarte <117078988+Felipe-M-Duarte@users.noreply.github.com>
2023-01-02 10:32:16 -0300

    Create SignupCustomerSchema.js

---
+++ database/schemas/SignupCustomerSchema.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+
+const SignupCustomer= new mongoose.Schema({
+    nome: {
+        type:String,
+        required:true,
+    },
+    email: {
+        type:String,
+    },
+    celular: {
+        type:String,
+    },
+    encontradopor: {
+        type:String,
+    },
+    instagram: {
+        type:String,
+    },
+    referenciadopor: {
+        type:String,
+    },
+    senha: {
+        type:String,
+    },
+    admin: {
+        type:Boolean,
+        default: false
+    },
+    criadoEm: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export default mongoose.model("SignupCustomer", SignupCustomer);
