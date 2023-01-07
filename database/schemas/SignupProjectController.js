4fd8cab2288a7f8d5485f1aa0543e442bd7a8d8e
Felipe-M-Duarte <117078988+Felipe-M-Duarte@users.noreply.github.com>
2023-01-07 09:58:48 -0300

    Create SignupProjectController.js

---
+++ database/schemas/SignupProjectController.js
@@ -0,0 +1,76 @@
+import {request, response} from 'express';
+import SignupProject from '../database/schemas/SignupProjectSchema.js'
+import generator from 'generate-password'
+
+
+
+class SignupProjectController {
+    async create(request, response){
+        try {
+            const {projname, domain, customer, discount, price, startdate, estimatedtime, file, functionalities, productservice, plataformmodule, objectives, criadoEm} = request.body;
+            //Project Id 
+            if(file){
+                console.log(file);
+            }
+
+            let col1pass = generator.generate({
+                length:4,
+                numbers: true,
+            })
+
+            let col2pass = generator.generate({
+                length:3,
+                numbers: true,
+            })
+
+            let col3pass = generator.generate({
+                length:2,
+                numbers: true,
+            })
+            
+            function catchletters(projname){
+                var names = projname.split(' ');
+                var initials = names[0].substring(0,1).toUpperCase();
+
+                if(names.length > 1) {
+                    initials += names[names.length - 1].substring(0,1).toUpperCase();
+                }
+
+                return initials
+            }
+
+            let projectid = col1pass + "-" + col2pass + '-' + col3pass + catchletters(projname);
+            //Project Id generator Closed
+
+
+            const signupprojectcontroller = await SignupProject.create({
+                projname,
+                projectid,
+                domain, 
+                customer, 
+                discount, 
+                price,
+                startdate, 
+                estimatedtime, 
+                functionalities, 
+                productservice, 
+                plataformmodule, 
+                objectives, 
+                criadoEm
+            });
+
+            return response.json(signupprojectcontroller);
+        }
+        catch (error) {
+            console.log(error);
+            return response.status(500).send({
+                error: "falhou em enviar mensagem",
+                mensagem: error
+            })
+
+        }
+    }
+
+}
+
+export default new SignupProjectController
