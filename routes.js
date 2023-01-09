093eb2500f6310bd10ffce1f364c1f0c11b83cc3
Felipe-M-Duarte <117078988+Felipe-M-Duarte@users.noreply.github.com>
2023-01-09 07:38:29 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,13 +3,6 @@ import contactController from './controller/contactController.js';
 import HomeContactController from './controller/HomeContactController.js';
 import SignupCustomerController from './controller/SignupCustomerController.js';
 import SignupProjectController from './controller/SignupProjectController.js';
-import Multer from 'multer'
-const multer = Multer({
-    storage: Multer.memoryStorage(),
-    limits: {
-      fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
-    }
-});
 
 
 const routes = Router();
@@ -20,7 +13,9 @@ routes.post("/contacthome", HomeContactController.create)
 
 routes.post("/admin/cadastrar/cliente", SignupCustomerController.create)
 
-routes.post("/admin/cadastrar/projeto",multer.single('file'),SignupProjectController.create)
+routes.post("/admin/cadastrar/projeto",SignupProjectController.create)
+
+routes.get("/contratos/:id", SignupProjectController.find)
 
 routes.get("/", (req, res) => {
     res.send('<h1>ROTA FUNCIONADO</h1>')
