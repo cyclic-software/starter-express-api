c7e006ef8f6256473cd7f7af31c11f0fc6b7c68b
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-01-30 15:33:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Hello world✋✋')
 })
 
 app.listen(PORT, (
-//     creating a server
     console.log(`Server is now Running on https://localhost:${PORT}`)
 ))
