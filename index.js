9c3762a2e12a600fa440c0a4a4d7f7caf4b32169
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-01-31 23:22:58 +0600

    Add query request

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+
+// Simple query request
+app.get("/query-req", (req, res) => {
+  const queryReq = req.query.req1;
+  res.send(`Your req: ${queryReq}`);
+});
+
+app.listen(process.env.PORT || 3000);
