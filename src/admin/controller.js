d2e153b5779a918becea38586a02001305720ae1
Arnauld Zeh <name@gmail.com>
2023-02-03 15:52:48 +0100

    some modifications

--- src/admin/controller.js
+++ src/admin/controller.js
@@ -1,9 +1,9 @@
 const { Identifiant, bloquer } = require("./models");
 
 //Create neww patient
-const createNewcardId = async (data) => {
+const createNewcardId = async (req, res) => {
   try {
-    const { cardId } = data;
+    const { cardId } = req.body;
 
     //checking if CardId belongs to the system
     //checking if patient already exists
@@ -16,15 +16,16 @@ const createNewcardId = async (data) => {
       cardId,
     });
     const createdcardId = await newcardId.save();
+    return res.status(200).json("cardId added successfully");
   } catch (error) {
     throw error;
   }
 };
 
 //block a user
-const bloquecardId = async (data) => {
+const bloquecardId = async (req, res) => {
   try {
-    const { cardId } = data;
+    const { cardId } = req.body;
 
     const existingcardId = await Identifiant.findOne({ cardId });
 
@@ -32,15 +33,15 @@ const bloquecardId = async (data) => {
     //checking if patient is already blocked
     const isBlockedcardId = await bloquer.findOne({ cardId });
     if (!existingcardId) {
-      throw Error("Identifiant non trouvé");
+      return res.status(404).json("Id not found");
     } else if (isBlockedcardId) {
-      throw Error("Cet Identifiant est deja bloqué");
+      return res.status(200).json("Id already blocked");
     } else {
       const newcardId = new bloquer({
         cardId,
       });
       const blockedcardId = await newcardId.save();
-      return "User blocked successfully";
+      return res.status(200).json("User blocked successfully");
     }
   } catch (error) {
     throw error;
--- src/admin/models.js
+++ src/admin/models.js
@@ -1,51 +1,21 @@
-const express = require("express");
-const router = express.Router();
-const { createNewcardId, bloquecardId } = require("./controller");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-//Ajouter une nouvelle carte de santé
-router.post("/cardId", async (req, res) => {
-  try {
-    //getting data from form body
-    let { cardId } = req.body;
-
-    cardId = cardId.trim();
-
-    if (!cardId) {
-      throw Error("Un ou plusieurs champs vides!!!");
-    } else {
-      // good credentials, create new user function in controller file
-      const newcardId = await createNewcardId({
-        cardId,
-      });
-      res.status(200).json(newcardId);
-    }
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
-//bloquer un utilisateur(patient/medecin) par l'Id de la carte de santé
-router.post("/bloquecardId", async (req, res) => {
-  try {
-    //getting data from form body
-    let { cardId } = req.body;
-
-    cardId = cardId.trim();
-
-    if (!cardId) {
-      throw Error("Un ou plusieurs champs vides!!!");
-    } else {
-      // good credentials, create new user function in controller file
-      const newcardId = await bloquecardId({
-        cardId,
-      });
-      res.status(200).json(newcardId);
-    }
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+//Identifiant Patients
+const IdSchema = new Schema({
+  cardId: {
+    type: String,
+  },
 });
 
-//Ajouter un nouveau medecin dans le systeme
-router.post("/new_doctor", async (req, res) => {});
+//Identifiants bloqué
+const bloquerSchema = new Schema({
+  cardId: {
+    type: String,
+  },
+});
 
-module.exports = router;
+const Identifiant = mongoose.model("cardIds", IdSchema);
+const bloquer = mongoose.model("blockedUsers", bloquerSchema);
+
+module.exports = { Identifiant, bloquer };
