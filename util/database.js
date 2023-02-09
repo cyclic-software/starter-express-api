4b4f917709b6ba73d6587ab8d5ab8fa2296f8348
sanjayvh <33938752+sanjayvh@users.noreply.github.com>
2023-02-10 00:15:51 +0530

    Update database.js
    Added environment varibales

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("ecommerce", "root", "Sanjay@678", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+  }
+);
 
 module.exports = sequelize;
