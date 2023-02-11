d3a1257cb624651c3798f16798f824160369104d
Abubaker <abubakeryounas6@gmail.com>
2023-02-11 12:01:40 -0800

    code added

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const forms = multer();
+const app = express();
+const port = process.env.PORT || 3030;
+
+// use json
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(forms.array());
+app.use(cors());
+app.use("/api/user", require("./routes/users"));
+
+app.get("/", (req, res) => {
+  res.send("Twitter Unfollower Tracker");
+});
+
+app.listen(port, () => {
+  console.log(`hookup app listening at http://localhost:${port}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const router = express.Router();
+const mongoStr =
+  "mongodb+srv://abubaker:testpassword@cluster0.zrjv5uj.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient } = require("mongodb");
+const bcrypt = require("bcrypt");
+const client = new MongoClient(mongoStr);
+const db = client.db("hookup");
+const { TwitterApi } = require("twitter-api-v2");
+const twitterClinet = new TwitterApi(
+  "AAAAAAAAAAAAAAAAAAAAABD3HAEAAAAAC68AmUmF4cU7RR%2FiZR%2BM3ddn5H4%3D80Eykin1Y3PcJN5BVWw2bLvm0EJ4wYVMdwap1MZDoTiBsZPTcq"
+);
+
+router.post("/signup", async (req, res) => {
+  const { email, password, name, phone, age } = req.body;
+  if (!email || !password) {
+    res.status(400).send("Email and password are required");
+  } else {
+    const user = await db.collection("users").findOne({ email });
+    if (user) {
+      res.status(400).send("User already exists");
+    } else {
+      const encryptedPassword = await bcrypt.hash(password, 10);
+      const result = await db
+        .collection("users")
+        .insertOne({ email, password: encryptedPassword, name, phone, age });
+      res.send({
+        status: true,
+        message: "User created successfully",
+      });
+    }
+  }
+});
+
+router.get("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send("Email and password are required");
+  } else {
+    const user = await db.collection("users").findOne({ email });
+    if (!user) {
+      res.status(400).send({
+        status: false,
+
+        message: "Wrong Credentials",
+      });
+    } else {
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        res.send({
+          status: true,
+          message: "User logged in successfully",
+          data: {
+            ...user,
+            password: undefined,
+          },
+        });
+      } else {
+        res.status(400).send({
+          status: false,
+
+          message: "Wrong Credentials",
+        });
+      }
+    }
+  }
+});
+
+router.get("/unfollowers", async (req, res) => {
+  let username = req.query.username;
+  if (!username) {
+    res.status(400).send("Username is required");
+  } else {
+    const tclient = twitterClinet.readOnly;
+    user = await client
+      .db("hookup")
+      .collection("followers")
+      .findOne({ username: username });
+    let userId = await tclient.v2.userByUsername(username);
+    let followers = await tclient.v2.followers(userId.data.id, {
+      max_results: 1000,
+    });
+    if (!user) {
+      await client.db("hookup").collection("followers").insertOne({
+        username: username,
+        followers: followers.data,
+      });
+      res.send({
+        status: false,
+        message:
+          "We dont have your followers list, but we have stored your followers to track them for the next time",
+      });
+    } else {
+      let oldFollowers = user.followers;
+      let newFollowers = followers.data;
+      let unfollowers = [];
+      for (let i = 0; i < oldFollowers.length; i++) {
+        let found = false;
+        for (let j = 0; j < newFollowers.length; j++) {
+          if (oldFollowers[i].id === newFollowers[j].id) {
+            found = true;
+            break;
+          }
+        }
+        if (!found) {
+          unfollowers.push(oldFollowers[i]);
+        }
+      }
+      await client
+        .db("hookup")
+        .collection("followers")
+        .updateOne(
+          { username: username },
+          { $set: { followers: newFollowers } }
+        );
+      res.send({
+        status: true,
+        message:
+          "Unfollowers are listed below, we have also stored your followers to track them for the next time",
+        data: unfollowers,
+      });
+    }
+
+    //res.send({ length: followers.data.length });
+  }
+});
+
+module.exports = router;
