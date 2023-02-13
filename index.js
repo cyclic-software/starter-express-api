510b568ce21e672f3087c81c077a1a76573dc98c
kamilpz <kamil.zytka@gmail.com>
2023-02-13 20:33:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 var text2png = require('text2png');
 
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
+app.set("views", path.join(__dirname))
+app.set("view engine", "ejs")
+
+app.get('/', function (req, res) {
     var text = req.query.text
     res.statusCode = 200
     res.setHeader('Content-Type', 'image/png')
     res.write(text2png(text, {output: 'buffer'}))
     res.end()
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, function(error) {
+    if(error) throw error
+    console.log("server started")
+})
