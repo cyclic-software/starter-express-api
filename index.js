807c3e3375c1717588fbde03e36aae130ed674e3
kamilpz <kamil.zytka@gmail.com>
2023-02-13 20:02:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+
+var text2png = require('text2png');
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    var text = req.query.text
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'image/png')
+    res.write(text2png(text, {output: 'buffer'}))
+    res.end()
 })
 app.listen(process.env.PORT || 3000)
