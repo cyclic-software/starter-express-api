404692159c2c50e24e00b8018c8d3a8e805e8fef
Abubaker <abubakeryounas6@gmail.com>
2023-02-21 16:04:29 -0800

    temp

--- index.js
+++ index.js
@@ -16,6 +16,44 @@ app.get("/", (req, res) => {
   res.send("Twitter Unfollower Tracker");
 });
 
+app.post("/calculate", (req, res) => {
+  let firstNumber = req.body.firstNumber;
+  let secondNumber = req.body.secondNumber;
+  const operator = req.body.operator;
+  if (!firstNumber || !secondNumber || !operator) {
+    res
+      .status(400)
+      .send({ status: false, message: "Please enter all the fields" });
+  } else {
+    firstNumber = parseInt(firstNumber);
+    secondNumber = parseInt(secondNumber);
+    if (operator == "1") {
+      const result = firstNumber + secondNumber;
+      res.send({
+        status: true,
+        message: "Result is " + result,
+        data: result,
+      });
+    } else if (operator == "2") {
+      const result = firstNumber - secondNumber;
+      res.send({
+        status: true,
+        message: "Result is " + result,
+        data: result,
+      });
+    } else if (operator == "3") {
+      const result = firstNumber * secondNumber;
+      res.send({
+        status: true,
+        message: "Result is " + result,
+        data: result,
+      });
+    } else {
+      res.status(400).send({ status: false, message: "Invalid operator" });
+    }
+  }
+});
+
 app.listen(port, () => {
   console.log(`hookup app listening at http://localhost:${port}`);
 });
