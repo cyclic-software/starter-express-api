3d2e1cfd1eded988149adc4aa3cbbe4f8562a40c
alexsandro cubas <alexsandro.cubas@pneufree.com.br>
2023-03-10 10:22:20 -0300

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ const dotenv = require("dotenv");
 const { PrismaClient } = require("@prisma/client");
 const port = 3005;
 
-const prisma = new PrismaClient();
-prisma
-    .$connect()
-    .then(() => {
-        const expressApp = express();
+// const prisma = new PrismaClient();
+// prisma
+//     .$connect()
+//     .then(() => {
+const expressApp = express();
 
-        expressApp.use(cors());
-        expressApp.use(express.json());
+expressApp.use(cors());
+expressApp.use(express.json());
 
-        require("./routes/index.js")(expressApp);
+require("./routes/index.js")(expressApp);
 
-        expressApp.listen(port, () => {
-            console.log(`Servidor Express iniciado na porta ${port}`);
-        });
-    })
-    .catch((err) => {
-        console.log("Erro ao conectar ao banco de dados: " + err);
-        process.exit(1);
-    });
+expressApp.listen(port, () => {
+    console.log(`Servidor Express iniciado na porta ${port}`);
+});
+// })
+// .catch((err) => {
+//     console.log("Erro ao conectar ao banco de dados: " + err);
+//     process.exit(1);
+// });
