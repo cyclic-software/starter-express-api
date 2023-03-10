0fd96d0ec1ac2e56046b686b363fa01f97ec54f5
alexsandro cubas <alexsandro.cubas@pneufree.com.br>
2023-03-10 10:12:28 -0300

    Feat: pf funciona

--- src/index.js
+++ src/index.js
@@ -2,26 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { PrismaClient } = require("@prisma/client");
-const port = 3001;
+const port = 3005;
 
 const prisma = new PrismaClient();
-dotenv.config();
 prisma
     .$connect()
     .then(() => {
         const expressApp = express();
 
-        // defina suas rotas e middlewares do Cyclic aqui
-
         expressApp.use(cors());
         expressApp.use(express.json());
-
-        // defina suas rotas e middlewares do Express aqui
 
         require("./routes/index.js")(expressApp);
 
         expressApp.listen(port, () => {
-            console.log(`Servidor Express iniciado na porta ${port}`);
+            console.log(`Servidor Express iniciado na porta a${port}`);
         });
     })
     .catch((err) => {
