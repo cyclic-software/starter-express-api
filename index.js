1cea19e08c9c85d471eb85b1472c6a52d998cfac
Nikhil.Kavathiya <nkavathiya@ginnovat.com>
2023-03-21 10:48:04 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+
+const getElementDetails = require("./getElemetDetails");
+
+app.get("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+
+app.get("/new", async function (req, res) {
+  let customSelector = `body p`;
+  await getElementDetails(
+    "https://nikhil-kavathiya-4a985.web.app/",
+    customSelector,
+    500,
+    700
+  )
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((e) => console.log(e));
+});
+app.listen(process.env.PORT || 3000);
