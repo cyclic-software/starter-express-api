26e8bd56d330bd66c704eeeacd37d90f293c213a
Carlos <andre021297@gmail.com>
2023-03-25 01:53:44 -0500

    models-created

---
+++ src/models/client.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const clientSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: uuidv4,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Client", clientSchema);
---
+++ src/models/order.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const orderSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: uuidv4,
+  },
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client",
+  },
+  orderItems: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  shippingAddress: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    extraInformation: {
+      type: String,
+    },
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  paymentResult: {
+    id: {
+      type: String,
+    },
+    status: {
+      type: String,
+    },
+    update_time: {
+      type: String,
+    },
+    email_address: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  isPaid: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  paidAt: {
+    type: Date,
+  },
+  isDelivered: {
+    type: Boolean,
+    required: true,
+  },
+  deliveredAt: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
