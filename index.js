55c2a1b7b1459174473877ed8f4b2f36c6c545b2
ehabatef205 <ehabmahmoud205@gmail.com>
2023-03-31 23:05:17 +0200

    edit

--- index.js
+++ index.js
@@ -4,28 +4,25 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const UserRoute = require('./routes/CourseRoute')
-
-mongoose.connect('mongodb+srv://Project:Project1234@project.2oh1pwy.mongodb.net/?retryWrites=true&w=majority',
-{useNewUrlParser: true,
-    useUnifiedTopology: true})
-
-const db = mongoose.connection
-
-db.on('error', (err) => {
-    console.log(err)
-})
-
-db.once('open', () => {
-    console.log('Database connection established!')
-})
-
 const app = express()
+const PORT = process.env.PORT || 5000
 app.use(express.json());
+
+mongoose.set('strictQuery', false)
+const connectDB = async () => {
+    try{
+const conn = await mongoose.connect(process.env.MONGO_URL)
+    console.log("MongoDB connected: ", conn.connection.host)
+    }catch(error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
 
 app.use('/api/course', UserRoute)
 
-const port = process.env.APP_PORT
-
-app.listen(port, () => {
-    console.log('Server is running on port ', port)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Listening on port ", PORT)
+    })
 })
