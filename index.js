6b6ed1d556b8915c521634b8a1c35353009280d9
Thatget <51475282+Thatget@users.noreply.github.com>
2023-04-15 17:10:15 +0000

    first

--- index.js
+++ index.js
@@ -11,25 +11,30 @@ const sheets = google.sheets({version: 'v4', auth: oauth2Client});
 
 app.get('/api', (req,res) => {
 
-    const spreadsheetId = '1yYIGMRGAvolZ9nMGx_U5RlKm5GHEARu59W2CS9Sy_iA';
-const sheetName = 'DASHBOARD';
-const range = 'A1:D10'; // Phạm vi các ô cần lấy giá trị
+    try {
+        const spreadsheetId = '1yYIGMRGAvolZ9nMGx_U5RlKm5GHEARu59W2CS9Sy_iA';
+        const sheetName = 'DASHBOARD';
+        const range = 'A1:D10'; // Phạm vi các ô cần lấy giá trị
 
-sheets.spreadsheets.values.get({
-  spreadsheetId: spreadsheetId,
-  range: sheetName + '!' + range,
-}, (err, res) => {
-  if (err) return console.log(`The API returned an error: ${err}`);
-  const rows = res.data.values;
-  if (rows.length) {
-    console.log('Name, Major:');
-    rows.forEach((row) => {
-      console.log(`${row[0]}, ${row[1]}`);
-    });
-  } else {
-    console.log('No data found.');
-  }
-});
+        sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: sheetName + '!' + range,
+        }, (err, res) => {
+        if (err) return console.log(`The API returned an error: ${err}`);
+        const rows = res.data.values;
+        if (rows.length) {
+            console.log('Name, Major:');
+            rows.forEach((row) => {
+            console.log(`${row[0]}, ${row[1]}`);
+            });
+        } else {
+            console.log('No data found.');
+        }
+        });
+        res.send('OK')
+    } catch (error) {
+        res.send({messge: error.messge})
+    }
 
 })
 
