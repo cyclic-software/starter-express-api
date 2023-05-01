ad4059ef5ded262962164c2822a3d973402feed6
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2023-05-01 13:28:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('done')
+})
+app.get('/hello',(req,res)=>{
+    res.send('hello world')
 })
 app.listen(process.env.PORT || 3000)
