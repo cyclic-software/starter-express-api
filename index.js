7497bd7c4e588302aa5fbfbc097725fc5c8ad5d8
parixit <parixit@pranshtech.com>
2023-05-07 18:05:21 +0530

    add route

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
 app.listen(process.env.PORT || 3000)
+
+app.get('/', function (req, res) {
+  console.log('recive');
+  res.json({
+    status:"recive"
+  });
+})
+app.get('/test', function (req, res) {
+  console.log('recive');
+  res.json({
+    status:"recive"
+  });
+})
