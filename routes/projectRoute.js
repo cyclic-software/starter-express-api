37bc524caa05ea9dbdbdf164673da777e46ae333
as7ro <orkhangk@code.edu.az>
2023-06-01 21:36:27 +0400

    Remove all files and directories

--- routes/projectRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import * as pageController from "../controllers/pageController.js";
-
-const router = express.Router();
-
-
-router.route("/").get(pageController.getProjectsPage)
-export default router
