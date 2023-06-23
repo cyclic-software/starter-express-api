7fb8389fa5f1f5754097f7e01cc1931cff565646
AminNurAzziz <aminnurazziz167@gmail.com>
2023-06-23 12:42:36 +0700

    first

---
+++ assets/js/pages/crm-opportunities.init.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var t=["#4fc6e1","#6658dd","#f7b84b","#f1556c","#1abc9c"],c=$("#status-chart").data("colors");c&&(t=c.split(","));function e(){$("#status-chart").sparkline([20,40,30,10,28],{type:"pie",width:"220",height:"220",sliceColors:t})}var i;e(),$(window).resize(function(t){clearTimeout(i),i=setTimeout(function(){e()},300)})});
