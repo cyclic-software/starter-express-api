7b5639441f77df1aa0d20b16e3c8f9e7e1b617cf
Trushit Gadhavi <78407658+trushitMidnight@users.noreply.github.com>
2023-07-02 15:36:19 +0530

    update

--- src/database/models/media.js
+++ src/database/models/media.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const {
   convertToSlug,
 } = require("../../utils");
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema; 
 
 const MediaSchema = new Schema(
   {
     media_type: { type: String,    enum: ['image', 'pdf','video'],    default:'image' },
     media_url: { type: String, default:''},
+    folder_name: { type: String, default:''},
     is_del: { type: Boolean, default: false },
   },
   {
