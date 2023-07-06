1d53c5c3faca4b3a02ffc8d5d5b4de0816cdba90
samuelprasad-yubi <samuel.prasad@go-yubi.com>
2023-07-06 21:56:39 +0530

    db

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo sam!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const url =
+  "mongodb+srv://Password:Password@cluster0.m9g1x.mongodb.net/maps?retryWrites=true&w=majority";
+
+mongoose.connect(url).then(() => {
+  console.log("connected to database");
+});
+
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo sam!");
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is Running");
+});
