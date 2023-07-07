a9d41700d841d14e5434766b205c88ff8163c0d9
Vexcon1 <53382767+Vexcon1@users.noreply.github.com>
2023-07-07 12:31:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+app.get('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.render("./index.html")
+})
+
+app.get('/status', (req, res) => {
+    console.log("Just got a request!")
+    res.json({"status": "main"})
 })
 app.listen(process.env.PORT || 3000)
