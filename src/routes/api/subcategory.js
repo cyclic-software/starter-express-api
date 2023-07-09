71834b357aa0d47c5b3bc3652aa986ac3eaf50e1
yousef alaa <yousefalaa2279@gmail.com>
2023-07-09 11:43:36 +0300

    sub category export

--- src/routes/api/subcategory.js
+++ src/routes/api/subcategory.js
@@ -8,3 +8,5 @@ router.get('/:id', SubCategoryRouter.get_subcategory_by_id)
 router.post('/', SubCategoryRouter.add_subcategory)
 router.put('/:id', SubCategoryRouter.update_subcategory)
 router.delete('/:id', SubCategoryRouter.delete_subcategory)
+
+module.exports = router
