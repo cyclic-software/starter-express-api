760fd620aef26ece05efd04e4efc744d12902642
Ramazanenesisik010 <ramazanenesisik010@gmail.com>
2023-07-20 21:58:37 +0300

    sa

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express')
+const discord = require('discord.js')
+const client = discord.client();
+
+const message1 = "<title>Mesaj Boş</title><p>Sa</p>";
+
+client.on('ready', () => {
+    console.log("Bon Running...");
+});
+
+client.on('message', message => {
+    if(message.content === "!help") {
+        message.reply("Komutlar: !help, !site, !degistir");
+    }
+    if(message.content === "!site") {
+        message.reply("Site : https://lovely-boot-dove.cyclic.app/");
+    }
+});
+
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Selam!')
+    res.send(message1)
 })
 app.listen(process.env.PORT || 3000)
+
+client.login('MTEzMTY0MDI5NjcyODQ0NDkyOA.GtTo9Q.0x3limSoGuqm-dC7xnEVcFABIfUdYhQVqe6Ye4');
