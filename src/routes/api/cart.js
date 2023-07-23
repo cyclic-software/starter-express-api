8fe7d46006998744ca227af05feebf1c54682b16
yousef alaa <yousefalaa2279@gmail.com>
2023-07-23 07:09:12 +0300

    auth

--- src/routes/api/cart.js
+++ src/routes/api/cart.js
@@ -4,13 +4,13 @@ const { checkToken } = require("../../auth/token_validation");
 
 const router = Router()
 
-router.get('/', CartController.get_carts)
-router.get('/:id', CartController.get_carts_by_id)
+router.get('/', checkToken, CartController.get_carts)
+router.get('/:id', checkToken, CartController.get_carts_by_id)
 router.post('/', checkToken, CartController.create_cart)
-router.put('/:id', CartController.update_cart)
-router.delete('/:id', CartController.remove_cart)
+router.put('/:id', checkToken, CartController.update_cart)
+router.delete('/:id', checkToken, CartController.remove_cart)
 router.get('/user', checkToken, CartController.get_carts_by_user_id)
-router.put('/plus/:id', CartController.add_one_quantity)
-router.put('/minus/:id', CartController.remove_one_quantity)
+router.put('/plus/:id', checkToken, CartController.add_one_quantity)
+router.put('/minus/:id', checkToken, CartController.remove_one_quantity)
 
 module.exports = router
