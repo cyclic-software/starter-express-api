2fd9e637b4dcddb2d727ee85e68781dcdc815a4b
samuelprasad-yubi <samuel.prasad@spocto.com>
2023-07-30 02:31:50 +0530

    db

--- helpers.js
+++ helpers.js
@@ -21,5 +21,7 @@ const cartoonNames = [
   "Patches",
 ];
 
-const getMongoLink = () => process.env.NODE_ENV ? "mongodb+srv://Password:Password@cluster0.m9g1x.mongodb.net/maps?retryWrites=true&w=majority" : 'mongodb://localhost:27017'; 
-module.exports = { cartoonNames , getMongoLink};
+const getMongoLink = () =>
+  "mongodb+srv://Password:Password@cluster0.m9g1x.mongodb.net/maps?retryWrites=true&w=majority";
+//: 'mongodb://localhost:27017';
+module.exports = { cartoonNames, getMongoLink };
