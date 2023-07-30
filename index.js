6cda29ea9eb54dc709ab768a44373e25e84ca7cb
Uday Hirapara <pnahir2251@gmail.com>
2023-07-30 19:04:19 +0530

    test

--- index.js
+++ index.js
@@ -3,27 +3,10 @@ const app = express()
 const { writeFileSync, readFileSync } = require('fs');
 app.use(express.json());
 const path = './db.json';
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3()
 
 app.listen(process.env.PORT || 3000)
 
 app.get('/', async function (req, res) {
-
-
-  await s3.putObject({
-    Body: JSON.stringify({ key: "value" }),
-    Bucket: "cyclic-good-handbag-hare-eu-central-1",
-    Key: "some_files/my_file.json",
-  }).promise()
-
-  // get it back
-  let my_file = await s3.getObject({
-    Bucket: "cyclic-good-handbag-hare-eu-central-1",
-    Key: "some_files/my_file.json",
-  }).promise()
-
-  console.log(JSON.parse(my_file))
 
   console.log('recive');
   res.json({
@@ -31,7 +14,7 @@ app.get('/', async function (req, res) {
   });
 })
 app.post('/led', async function (req, res) {
-  var body = req.body;
+  var body = req.body; 
 
   await writeFileSync(path, JSON.stringify(body, null, 2), 'utf8');
 
@@ -46,7 +29,7 @@ app.get('/led', async function (req, res) {
 
     var data = await readFileSync('./db.json');
     data = JSON.parse(data);
-
+    data.data.red_led = process.env.redled;
     res.json({
       status: true,
       data: data
