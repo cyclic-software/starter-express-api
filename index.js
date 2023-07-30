5aa28b9d93f5e51a8a722adb217d95ec1b7cf77c
Uday Hirapara <pnahir2251@gmail.com>
2023-07-30 18:54:22 +0530

    test aws bucket

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const app = express()
 const { writeFileSync, readFileSync } = require('fs');
 app.use(express.json());
 const path = './db.json';
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
 app.listen(process.env.PORT || 3000)
 
 app.get('/', async function (req, res) {
+
+
+  await s3.putObject({
+    Body: JSON.stringify({ key: "value" }),
+    Bucket: "cyclic-good-handbag-hare-eu-central-1",
+    Key: "some_files/my_file.json",
+  }).promise()
+
+  // get it back
+  let my_file = await s3.getObject({
+    Bucket: "cyclic-good-handbag-hare-eu-central-1",
+    Key: "some_files/my_file.json",
+  }).promise()
+
+  console.log(JSON.parse(my_file))
 
   console.log('recive');
   res.json({
@@ -14,7 +31,7 @@ app.get('/', async function (req, res) {
   });
 })
 app.post('/led', async function (req, res) {
-  var body = req.body; 
+  var body = req.body;
 
   await writeFileSync(path, JSON.stringify(body, null, 2), 'utf8');
 
