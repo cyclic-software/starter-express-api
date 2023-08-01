f74301c137f2ec88df58e4a536a9272310a1d135
Trushit gadhavi <trushitgadhavi99133@gmail.com>
2023-08-01 17:45:01 +0530

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -63,6 +63,8 @@ module.exports.paginateResults = async (
   matcharray = [],
   sortarray = {}
 ) => {
+  console.log("matcharray")
+  console.log(JSON.stringify(matcharray))
   var pipeline = [];
 
   if (
