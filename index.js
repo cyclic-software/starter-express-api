987846e514a3a4d2fba7813cfe4428f29edd048c
Vexcon1 <theofletcher82@gmail.com>
2023-08-23 12:38:17 -0400

    master

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const path = require('path');
 const app = express()
+const main = express()
+
 app.get('/', (req, res) => {
     console.log("Just got a request!")
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/main/index.html'));
 })
 
 app.get('/status', (req, res) => {
     console.log("Just got a request!")
     res.json({"status": "main"})
 })
+
 app.listen(process.env.PORT || 3000)
