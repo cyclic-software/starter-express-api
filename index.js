6b7fc8aca09adf8966e4b47dd1cf839eb7b3da8c
Ramazan Enes IŞIK <114609990+Ramazanenesisik010@users.noreply.github.com>
2023-08-26 09:32:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer");
 
+const email = mail.createTransport({
+    host:"smtp.office365.com",
+    port:587,
+    auth:{
+        user:"steamcraftteams@hotmail.com",
+        pass:"Er030303"
+    }
+});
+let message = "StealCraft Teams Tarafından Gönderirdi";
+const mailOptions = {
+    from: "steamcraftteams@hotmail.com",
+    to: "muhammedemirisik04@gmail.com",
+    subject:"StealCraft Feedback Info",
+    text: message
+};
+
+email.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
 let users = [
     {id:0,name:"Ramazan Enes ISIK",pass:"Er031202",active:true}
 ]
@@ -20,9 +44,7 @@ app.get("/", function(req,res) {
 });
 
 app.get("/feedback=:id", function(req,res) {
-    res.render("index",{
-        user: users
-    });
+    const username = req.params.id.split("username=")[1];
 });
 
 app.get("/ugt", function(req,res) {
