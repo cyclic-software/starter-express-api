9f7bcfabdb7f8534b4719fd11e8518fbee01869c
Rohitkumar Jha <58877946+rohitkumar-jha@users.noreply.github.com>
2023-08-27 14:29:38 +0530

    Add files via upload

---
+++ maths/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = process.env.Port || 9001;
+
+app.use("/", (req, res) => {
+  return res.json({ messgae: "Hello Dude I am here" });
+});
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
