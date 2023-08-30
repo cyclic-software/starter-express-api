5d16ed51f8526ed34147a75828cd1929031ab0ef
Rohitkumar Jha <58877946+rohitkumar-jha@users.noreply.github.com>
2023-08-31 00:21:40 +0530

    logger error
    commented logger file

--- maths/src/Common/logger.js
+++ maths/src/Common/logger.js
@@ -1,11 +1,13 @@
 const logger = require('./common-logger')
 
 const infoLog = (message) => {
-    return logger.infoLogger.info(message);
+    //return logger.infoLogger.info(message);
+    return
 }
 
 const errorLog = (message) => {
-    return logger.errorLogger.error(message);
+    //return logger.errorLogger.error(message);
+    return;
 }
 
 module.exports = { infoLog, errorLog }
