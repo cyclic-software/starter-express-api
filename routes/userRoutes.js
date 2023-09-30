6d4ad254df6357e38f1071001bda933f4c59b359
Amin Nur Azziz <93433929+AminNurAzziz@users.noreply.github.com>
2023-09-30 14:06:10 +0700

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ router.route('/register')
     .get(UserController.renderRegister)
     .post(UserController.register);
 
-router.route('/login')
+router.route('/')
     .get(UserController.renderLogin)
     .post(passport.authenticate('local', { failureFlash: 'Passwordmu salah bro', failureRedirect: '/login' }), UserController.login);
 
