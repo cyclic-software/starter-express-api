52042731e61217840b264e23fe38451f99e84be2
amitsharma-hs01 hs <amitsharma.hs01@gmail.com>
2023-10-12 12:08:36 +0000

    init2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express"
 const app = express()
+const port=process.env.PORT||5000
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(port,()=>{
+    console.log(`server on port ${port}`)
+})
