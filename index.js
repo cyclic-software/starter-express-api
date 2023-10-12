81ad8a87b22ed9c064ea3973a316aa3517fd497e
amitsharma-hs01 hs <amitsharma.hs01@gmail.com>
2023-10-12 13:03:32 +0000

    starting development

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express"
 import path from "path"
-import { fileURLToPath } from "url"
-import { dirname } from "path"
+// import { fileURLToPath } from "url"
+// import { dirname } from "path"
 
 const app = express()
 const port=process.env.PORT||5000
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=dirname(__filename)
+// const __filename=fileURLToPath(import.meta.url);
+// const __dirname=dirname(__filename)
 
-app.use(express.static(path.join(__dirname,"./client/build")))
-app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
+// app.use(express.static(path.join(__dirname,"./client/build")))
+// app.use("*",function(req,res){
+//     res.sendFile(path.join(__dirname,"./client/build/index.html"))
+// })
 app.get('/', (req, res) => {
     console.log("Just got a request!")
     res.send("hey")
