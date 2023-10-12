42bcef8ada2b7a55f98d4c80f809e6ebb9b698e0
amitsharma-hs01 hs <amitsharma.hs01@gmail.com>
2023-10-12 12:35:58 +0000

    init5

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express"
 import path from "path"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
 
 const app = express()
 const port=process.env.PORT||5000
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=dirname(__filename)
 app.get('/', (req, res) => {
     console.log("Just got a request!")
     res.sendFile(path.join(__dirname,"./client/public/index.html"))
