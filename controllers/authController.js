dcfac2169ac7de6b1e64c820cf7e48e8733eaf11
amitsharma-hs01 hs <amitsharma.hs01@gmail.com>
2023-10-12 19:46:52 +0000

    login route complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from "../models/userModel.js";
 import { comparePassword, hashPassword } from "../helpers/authhelpers.js";
+import JWT from "jsonwebtoken";
 
 export const registerController=async (req,res)=>{ 
     try{
@@ -30,3 +31,44 @@ export const registerController=async (req,res)=>{
       })
     }
   };
+
+  //login controller
+
+  export const loginController = async (req,res)=>{
+    try {
+     const {email,password}=req.body
+     const user=await userModel.findOne({email})
+     if(!user){
+       return res.status(200).send({
+         success:false,
+         message:"No user found"
+       })
+     }
+     const match=await comparePassword(password,user.password)
+     if(!match){
+       return res.status(200).send({
+         success:false,
+         message:"Invalid Password"
+       })
+     } 
+     const token =  JWT.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"})
+     res.send({
+       success:true,
+       message:"loggin successfull",
+       token,
+       user:{ 
+       id:user._id,
+       name:user.name,
+       email:user.email,
+       number:user.number,
+       role:user.role?user.role:"0"
+       }
+     })
+      } catch (error) {
+       console.log(error)
+     return res.status(500).send({
+       success:false,
+       message:"login failed"
+     }) 
+    }
+ }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,9 @@
 import express from "express"
-import { registerController } from "../controllers/authController.js"
+import { registerController,loginController } from "../controllers/authController.js"
 
 export const authRouter=express.Router()
 
 authRouter.post("/register",registerController)
+
+authRouter.post("/login",loginController)
 
