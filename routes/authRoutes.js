96ac223597de4d115c273827cb9a1bb33703a006
amitsharma-hs01 hs <amitsharma.hs01@gmail.com>
2023-10-13 05:31:23 +0000

    auth middleware setup complete

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,20 @@
 import express from "express"
 import { registerController,loginController } from "../controllers/authController.js"
+import { requireSignin ,isAdmin} from "../middlewares/authMiddleware.js"
 
 export const authRouter=express.Router()
 
 authRouter.post("/register",registerController)
 
 authRouter.post("/login",loginController)
+
+authRouter.get('/authenticate',requireSignin,((req,res)=>{
+    res.status(200).send({ok:true})
+}))
+
+authRouter.post("/admin-auth",requireSignin,isAdmin,(req,res)=>{
+    res.status(200).send({
+        ok:true
+    })
+})
 
