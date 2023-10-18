9c6e4f67fdc7651b8bf240a722719b3eb1b1e592
park <zparkasv2@gmail.com>
2023-10-18 16:03:53 +0700

    update api connection database

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express')
 const app = express()
+require("dotenv/config");
+const { Client } = require('pg');
+
+const client = new Client({
+    user: process.env.USER_NAME,
+  host: process.env.POSTGRESQL_URL,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+  database: process.env.USER_NAME
+});
+// เชื่อมต่อกับฐานข้อมูล
+client.connect()
+  .then(() => {
+    console.log('เชื่อมต่อกับฐานข้อมูลสำเร็จ');
+})
+  .catch(err => {
+    console.error('เกิดข้อผิดพลาดในการเชื่อมต่อกับฐานข้อมูล:', err);
+});
+
+
 app.all('/', (req, res) => {
     
     res.send('Hello World!')
@@ -9,4 +29,29 @@ app.post('/api/scholarship/login', (req, res) => {
     
     res.send({result : {accessToken: 'Login success'} })
 })
-app.listen(process.env.PORT || 3000)
+
+app.listen(4000)
+
+app.get('/api/scholarship/classYearType',(req,res)=>{
+    const query = 'SELECT * FROM class_year_type';
+    
+    // ส่งคำสั่ง SQL ไปยังฐานข้อมูล
+    client.query(query)
+      .then(result => {
+        const rows = result.rows;
+        res.send({result:rows})
+      })
+      .catch(err => {
+        console.error('เกิดข้อผิดพลาดในการค้นหาข้อมูล:', err);
+      })
+    //   .finally(() => {
+    //     // ปิดการเชื่อมต่อเมื่อไม่ได้ใช้งาน
+    //     client.end()
+    //       .then(() => {
+    //         console.log('การเชื่อมต่อถูกปิด');
+    //       })
+    //       .catch(err => {
+    //         console.error('เกิดข้อผิดพลาดในการปิดการเชื่อมต่อ:', err);
+    //       });
+    //   });
+})
