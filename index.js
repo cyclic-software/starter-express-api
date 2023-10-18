a49c59dec069be99d08022073e1711130064da54
park <zparkasv2@gmail.com>
2023-10-18 10:57:06 +0700

    Test backend

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    
+    res.send('Hello World!')
+})
+
+app.post('/api/scholarship/login', (req, res) => {
+    
+    res.send({result : {accessToken: 'Login success'} })
 })
 app.listen(process.env.PORT || 3000)
