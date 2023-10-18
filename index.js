76a00ccf03332143c06d20d256007c7130471cdb
Leonel Goncalves <devok.software@gmail.com>
2023-10-18 09:54:24 +0200

    Test local storage from cyclic

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var cors = require("cors");
 const cron = require("node-cron");
 const request = require("request");
 const moment = require("moment");
-const fs = require("fs");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
 app.use(
   cors({
@@ -27,6 +28,66 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://draft-bola-ao-ar.onrender.com",
 ];
+
+const options = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+    "x-rapidapi-key": process.env.API_KEY,
+  },
+};
+
+app.get("/standings2", async (req, res) => {
+  console.log("GET - Requesting standings...");
+
+  let standingsFile = await s3
+    .getObject({
+      Bucket: "cyclic-elated-tuxedo-mite-eu-central-1",
+      Key: "standings2.json",
+    })
+    .promise();
+
+  if (standingsFile) {
+    const jsonFile = JSON.parse(data);
+    const fileDate = moment(jsonFile.lastUpdate, "DD-MM-YYYY HH:mm:ss");
+    if (fileDate.add(1, "hour").isBefore(moment())) {
+      console.log("Updating Standings...");
+      requestStandingAndSave();
+    } else {
+      res.send(jsonFile);
+    }
+  } else {
+    console.log("File does not exists. Creating a new one.");
+    requestStandingAndSave();
+  }
+});
+
+function requestStandingAndSave() {
+  request(
+    "https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022",
+    options,
+    async function (error, response, body) {
+      console.log("Received response from API.");
+      if (!error && response.statusCode == 200) {
+        json = JSON.parse(response.body);
+        json.lastUpdate = moment().format("DD-MM-YYYY HH:mm:ss");
+
+        let fileInStringFormat = JSON.stringify(json);
+
+        await s3
+          .putObject({
+            Body: fileInStringFormat,
+            Bucket: "cyclic-elated-tuxedo-mite-eu-central-1",
+            Key: "standings2.json",
+          })
+          .promise();
+        res.send(fileInStringFormat);
+      } else {
+        console.log(response.body);
+      }
+    }
+  );
+}
 
 app.get("/standings", async (req, res) => {
   console.log("GET - Requesting standings...");
