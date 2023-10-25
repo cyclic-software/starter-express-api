1bc6c84439831fcfc49e72c74fe6659750ee3f00
park <zparkasv2@gmail.com>
2023-10-25 18:01:48 +0700

    create API Addscholarship

---
+++ controller/addScholarship/handler.js
@@ -0,0 +1,56 @@
+const executeQuery = require("../../utils/pool_connections");
+const { v4: uuidv4 } = require("uuid");
+
+const handlerAddscholarship = (req, res) => {
+  const {
+    scholarship_name,
+    scholarship_year,
+    scholarship_grade,
+    description,
+    class_type_id,
+    start_date,
+    end_date,
+    scholarship_type_id,
+    is_active,
+    color_tag,
+    scholarship_codition,
+    scholarship_qualification,
+  } = req.body;
+  const scholarship_id = uuidv4();
+
+  const query = `INSERT INTO
+  scholarship_info (
+       scholarship_id,
+      scholarship_name,
+      scholarship_year,
+      scholarship_grade,
+      class_type_id,
+      scholarship_type_id,
+      start_date,
+      end_date,
+      description,
+      is_active,
+      color_tag,
+      scholarship_condition,
+      scholarship_qualification
+  )
+VALUES
+  (
+      '${scholarship_id}',
+      '${scholarship_name}',
+      '${scholarship_year}',
+      '${scholarship_grade}',
+      '${class_type_id}',
+      '${scholarship_type_id}',
+      '${start_date}',
+      '${end_date}',
+      '${description}',
+      '${is_active}',
+      '${color_tag}',
+      '${scholarship_codition}',
+      '${scholarship_qualification}'
+  )`;
+  executeQuery(query, res);
+};
+
+module.exports = handlerAddscholarship;
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const app = express();
 const handleclassYearType = require("./controller/classYearType");
 const handleScholarshipType = require("./controller/scholarshipType");
+const handleAddscholarship = require("./controller/addScholarship");
 
+app.use(express.json());
 app.use(cors({ origin: "*", credentials: true }));
 
 app.post("/api/scholarship/login", (req, res) => {
@@ -12,5 +14,7 @@ app.post("/api/scholarship/login", (req, res) => {
 
 app.get("/api/scholarship/classYearType", handleclassYearType),
 app.get("/api/scholarship/scholarshipType", handleScholarshipType),
+
+app.post("/api/scholarship/addScholarship", handleAddscholarship)
 
 app.listen(4000,()=>{console.log('Running on Port 4000')});
