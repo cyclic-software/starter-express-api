55fb4ffd2705f31e93a359fb3b73d2770d76b8d3
Afonso Faria <73140434+AfonsoFaria20s@users.noreply.github.com>
2023-11-04 19:31:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+app.all('/test', (req, res) => {
     console.log("Just got a request!")
-    res.send('Working!!')
+    res.send('Hello User!')
 })
 app.listen(process.env.PORT || 3000)
