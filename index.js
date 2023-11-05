a1b8c02d9c080d77944aae565bb181f8008cd6b0
Afonso Faria <73140434+AfonsoFaria20s@users.noreply.github.com>
2023-11-05 11:40:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 app.get('/test', (req, res) => {
     console.log("Just got a request!")
+    let object = {
+    	name : "Petya",
+      	age : 15
+    };
     
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Origin', 'https://smartquiz.pages.dev/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.send('Hello User!')
+    
+    res.send(object)
 })
 app.listen(process.env.PORT || 3000)
