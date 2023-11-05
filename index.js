21c6477da5d8ce48e9e8532aabffe4a42bf0595e
Afonso Faria <73140434+AfonsoFaria20s@users.noreply.github.com>
2023-11-05 11:42:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ app.get('/test', (req, res) => {
       	age : 15
     };
     
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Origin', 'https://smartquiz.pages.dev/');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
+    res.header("Access-Control-Allow-Origin", "*");
     res.send(object)
 })
 app.listen(process.env.PORT || 3000)
