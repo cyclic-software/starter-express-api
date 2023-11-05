3909f035d21098855e36d6425d64792329871fc3
Afonso Faria <73140434+AfonsoFaria20s@users.noreply.github.com>
2023-11-05 12:07:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors());
+
 app.get('/test', (req, res) => {
     console.log("Just got a request!")
     let object = {
-    	name : "Petya",
-      	age : 15
+        name: "Petya",
+        age: 15
     };
-    
+
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.send(object)
+    res.send(object);
 })
 app.listen(process.env.PORT || 3000)
