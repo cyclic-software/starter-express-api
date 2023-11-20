0efd9ccc4799e96631bbf1abf17c1ed09c9ddefc
Sinelizwi Ntaku <83240229+Snezoh97@users.noreply.github.com>
2023-11-20 20:54:04 +0200

    Delete node_modules directory

--- node_modules/canvas/src/InstanceData.h
+++
@@ -1,8 +0,0 @@
-#include <napi.h>
-
-struct InstanceData {
-  Napi::FunctionReference ImageBackendCtor;
-  Napi::FunctionReference PdfBackendCtor;
-  Napi::FunctionReference SvgBackendCtor;
-  Napi::FunctionReference CanvasCtor;
-};
