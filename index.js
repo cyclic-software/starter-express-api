2a51a9e4fcb6142c30134645d724621377f208b4
JP <jonathankbyers@gmail.com>
2023-12-29 16:06:13 -0500

    First live Full Stack App with MongoDB

--- index.js
+++ index.js
@@ -1,39 +1,122 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
 const cors = require('cors')
-
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+const PORT = 3000
 app.use(cors())
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 
-const rappers = {    
-    '21 savage': {
-        'age': 29,
-        'birthName': 'Sheyaa Bin Abraham-Joseph',
-        'birthLocation': 'London, England'
-    },
-    'chance the rapper': {
-        'age': 29,
-        'birthName': 'Chancelor Bennett',
-        'birthLocation': 'Chicago, Ill'
-    },
-    'unknown': {
-        'age': 0,
-        'birthName': 'unknown',
-        'birthLocation': 'unknown'
-    }
-}
+let db,
+    dbConnectionStr = 'mongodb+srv://jonathankbyers:02S6kDc3hv9J0oMX@testingcluster.wdoczch.mongodb.net',
+    dbName = 'rap'
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+
+app.get('/',async (request, response)=>{
+    const todoItems = await db.collection('todos').find().toArray()
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    // db.collection('todos').find().toArray()
+    // .then(data => {
+    //     db.collection('todos').countDocuments({completed: false})
+    //     .then(itemsLeft => {
+    //         response.render('index.ejs', { items: data, left: itemsLeft })
+    //     })
+    // })
+    // .catch(error => console.error(error))
 })
 
-app.get('/api/:name',(req, res) =>{
-    const rapperName = req.params.name.toLowerCase()
-    //console.log(rappers[rapperName].birthName)
-    if(rappers[rapperName]){
-        res.json(rappers[rapperName])
-    }else res.json(rappers['unknown'])
+app.post('/addTodo', (request, response) => {
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    .then(result => {
+        console.log('Todo Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
 })
+
+app.put('/markComplete', (request, response) => {
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        $set: {
+            completed: true
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked Complete')
+        response.json('Marked Complete')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.put('/markUnComplete', (request, response) => {
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        $set: {
+            completed: false
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked Complete')
+        response.json('Marked Complete')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteItem', (request, response) => {
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    .then(result => {
+        console.log('Todo Deleted')
+        response.json('Todo Deleted')
+    })
+    .catch(error => console.error(error))
+
+})
+
+
+// const rappers = {    
+//     '21 savage': {
+//         'age': 29,
+//         'birthName': 'Sheyaa Bin Abraham-Joseph',
+//         'birthLocation': 'London, England'
+//     },
+//     'chance the rapper': {
+//         'age': 29, 
+//         'birthName': 'Chancelor Bennett',
+//         'birthLocation': 'Chicago, Ill'
+//     },
+//     'unknown': {
+//         'age': 0,
+//         'birthName': 'unknown',
+//         'birthLocation': 'unknown'
+//     }
+// }
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
+
+// app.get('/api/:name',(req, res) =>{
+//     const rapperName = req.params.name.toLowerCase()
+//     //console.log(rappers[rapperName].birthName)
+//     if(rappers[rapperName]){
+//         res.json(rappers[rapperName])
+//     }else res.json(rappers['unknown'])
+// })
 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+const deleteBtn = document.querySelectorAll('.fa-trash')
+const item = document.querySelectorAll('.item span')
+const itemCompleted = document.querySelectorAll('.item span.completed')
+
+Array.from(deleteBtn).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
+})
+
+Array.from(item).forEach((element)=>{
+    element.addEventListener('click', markComplete)
+})
+
+Array.from(itemCompleted).forEach((element)=>{
+    element.addEventListener('click', markUnComplete)
+})
+
+async function deleteItem(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markUnComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markUnComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
