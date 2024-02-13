3827da1f559e4e722d6bcda5ff297b2b2f914a9c
ss6239620 <ss6239620@gmail.com>
2024-02-13 22:29:08 +0530

    made a test endpoint

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.get('/test',(res,req)=>{
+    res.send('hola')
+})
 app.listen(process.env.PORT || 3000)
