62e65d3a8f905cbf2d556cb08fc95390d82e16d5
ss6239620 <66192687+ss6239620@users.noreply.github.com>
2024-02-14 09:52:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yoooo!')
 })
+app.get('/test',(req,res)=>{
+    res.send('hello')
+})
 app.listen(process.env.PORT || 3000)
