db382ffaaa252ecc125602c1067cacf4e1461a1c
abhishek-reddy-1999 <132802759+abhishek-reddy-1999@users.noreply.github.com>
2024-02-24 11:08:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Dev Branch!')
+    
+    res.send('............................Server is Started........................... ')
 })
 app.listen(process.env.PORT || 3000)
